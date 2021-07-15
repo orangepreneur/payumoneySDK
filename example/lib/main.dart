8086eb2f78adb8ecc1e88622e827e28378e6d6d5
Mukesh Joshi <contact@orangewp.com>
2021-07-16 00:50:03 +0530

    readme updated

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -6,6 +6,7 @@ void main() {
 }
 
 class MyApp extends StatefulWidget {
+  // Example app to test the payumoney plugin
   @override
   _MyAppState createState() => _MyAppState();
 }
@@ -27,11 +28,11 @@ class _MyAppState extends State<MyApp> {
           child: ElevatedButton(
             child: Text("Make Payment"),
             onPressed: () async {
+              // // Every Transaction should have a unique ID. I am using timestamp as transactionid. Because its always unique :)
               String orderId = DateTime.now().millisecondsSinceEpoch.toString();
+              // Amount is in rs. Enter 100 for Rs100.
               final String amount = "1";
 
-              // Amount is in rs. Enter 100 for Rs100.
-              // Every Transaction should have a unique ID
               // Phone Number should be 10 digits. Please validate it before passing else it will throw error.
               // hashUrl is required. check github documentation for nodejs code.
               var response = await PayumoneyProUnofficial.payUParams(
@@ -50,9 +51,10 @@ class _MyAppState extends State<MyApp> {
                   userCredentials: 'merchantKey:test@example.com',
                   showLogs: true,
                   userPhoneNumber: '9999999999');
-
+              // handling success response
               if (response['status'] == PayUParams.success)
                 handlePaymentSuccess(amount);
+              // handling failure response
               if (response['status'] == PayUParams.failed)
                 handlePaymentFailure(amount, response['message']);
             },
@@ -62,11 +64,13 @@ class _MyAppState extends State<MyApp> {
     );
   }
 
+// Function to implement business login on payment success
   handlePaymentSuccess(String amount) {
     print("Success");
     // Implement your logic here for successful payment.
   }
 
+// Function to implement business login on payment failure
   handlePaymentFailure(String amount, String error) {
     print("Failed");
     print(error);
--- lib/payumoney_pro_unofficial.dart
+++ lib/payumoney_pro_unofficial.dart
@@ -2,9 +2,12 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+// class to Implement PayUMoney CheckoutPro SDK in Android & iOS App
 class PayumoneyProUnofficial {
+  // method channel allow us to run platform specific code.
   static const MethodChannel _channel =
       const MethodChannel('payumoney_pro_unofficial');
+  // function to build payment params for payumoney. More details can be found at payumoney documentation
   static Future<Map<dynamic, dynamic>> payUParams({
     required String amount,
     required bool isProduction,
@@ -24,6 +27,7 @@ class PayumoneyProUnofficial {
     required String userCredentials,
   }) async {
     try {
+      // passing data to platform. expected to get <string,dynamic> Map in return
       final data = await _channel.invokeMethod('payUParams', {
         'amount': amount,
         'isProduction': isProduction,
@@ -44,6 +48,7 @@ class PayumoneyProUnofficial {
       });
       return data;
     } catch (error) {
+      // debuging error in case of payment failure.
       debugPrint(error.toString());
       final errorResponse = {"status": "failed", "message": "payment canceled"};
       return errorResponse;
@@ -51,6 +56,7 @@ class PayumoneyProUnofficial {
   }
 }
 
+// Class to abstract some strings and make code look clean for users.
 class PayUParams {
   static const successURL =
       "https://www.payumoney.com/mobileapp/payumoney/success.php";
